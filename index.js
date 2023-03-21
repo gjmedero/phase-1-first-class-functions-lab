b0a1ce06a455776ca25f754a1fdcfff5b16439f9
Gilberto Medero <gjmedero@gmail.com>
2023-03-21 16:48:19 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0,2);
+}
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(number) {
+    return function (number) {
+        return number * number;
+    }
+}
+
+function fareDoubler(number) {
+    const numberMulti= 2 * number;
+    return numberMulti;
+}
+
+function fareTripler(number) {
+    const numberTriple = 3 * number;
+    return numberTriple;
+}
+
+function selectDifferentDrivers(arrayOfDrivers, driverSeclection) {
+    if (driverSeclection === returnFirstTwoDrivers) {
+        return returnFirstTwoDrivers(arrayOfDrivers);
+    } 
+    else {
+        return returnLastTwoDrivers(arrayOfDrivers);   
+    }
+}
